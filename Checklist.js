d7afc9e9beb9de2bdd66ce9438999bb3a24c8dab
Kashaf Nek <138366744+Kashafnek@users.noreply.github.com>
2023-10-09 17:39:59 +0500

    Checklist App.js

--- Checklist.js
+++ Checklist.js
@@ -1,4 +1,3 @@
-// Constant variables
 const addChecklist = document.getElementById('addChecklist');
 const showAddChecklist = document.getElementById('showAddChecklist');
 const input = addChecklist.querySelector('input');
@@ -6,18 +5,15 @@ const ul = document.querySelector('ul');
 
 addChecklist.style.display = "none";
 
-// Function for showing inputs
 function showChecklist() {
     showAddChecklist.style.display = "none";
     addChecklist.style.display = "";
     
 }
 
-// Event listener for clicking the checkmark icon
 const checkmark = addChecklist.querySelector('a');
 checkmark.addEventListener('click', addListItem);
 
-// Function to add a new list item
 function addListItem() {
     const text = input.value.trim();
     
@@ -35,13 +31,11 @@ function addListItem() {
     }
 }
 
-// Function to save list items to local storage
 function saveListItems() {
     const listItems = Array.from(ul.getElementsByTagName('li')).map(li => li.textContent);
     localStorage.setItem('items', JSON.stringify(listItems));
 }
 
-// Function to load list items from local storage
 function loadListItems() {
     const savedItems = localStorage.getItem('items');
     
@@ -55,13 +49,11 @@ function loadListItems() {
     }
 }
 
-// Load list items from local storage when the page loads
 loadListItems();
 
-// Function to toggle the id attribute when an <li> is clicked
 function toggleIdAttribute(event) {
     const li = event.target;
-    const currentId = li.getAttribute('id'); // Get the current id
+    const currentId = li.getAttribute('id');
     
     if (currentId === 'done') {
         li.removeAttribute('id');
@@ -76,8 +68,6 @@ for (const li of liElements) {
     li.addEventListener('click', toggleIdAttribute);
 }
 
-
-// Reset local storage function
 function resetList() {
     
     if (confirm("Do you want to reset the list?")) {
